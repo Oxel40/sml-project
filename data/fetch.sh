b9756d431c7e29b33b99fa42916ae1e9151a5770
Oxel40 <Oxel40@users.noreply.github.com>
2024-01-03 13:52:48 +0100

    wip

--- data/fetch.sh
+++ data/fetch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-STATION_ID=98230
+STATION_ID=97100
 
-for PARAM in {0..30}; do
-	curl -f "https://opendata-download.smhi.se/stream?type=metobs&parameterIds=$PARAM&stationId=$STATION_ID&period=corrected-archive" -o "$STATION_ID-$PARAM-archive.csv"
-	curl -f "https://opendata-download.smhi.se/stream?type=metobs&parameterIds=$PARAM&stationId=$STATION_ID&period=latest-months" -o "$STATION_ID-$PARAM-latest.csv"
+mkdir -p "./tmp"
+
+for PARAM in {0..100}; do
+	curl -f "https://opendata-download.smhi.se/stream?type=metobs&parameterIds=$PARAM&stationId=$STATION_ID&period=corrected-archive" -o "./tmp/$STATION_ID-$PARAM-archive.csv"
+	curl -f "https://opendata-download.smhi.se/stream?type=metobs&parameterIds=$PARAM&stationId=$STATION_ID&period=latest-months" -o "./tmp/$STATION_ID-$PARAM-latest.csv"
 done
+
+grep -il "1 gång/tim;" ./tmp/*.csv | xargs -I % cp % .
+sed -i '/Datum/,$!d' *.csv
--- temperature-feature-pipeline-daily.py
+++ temperature-feature-pipeline-daily.py
@@ -1,24 +1,48 @@
 import os
+import subprocess
 import pandas as pd
 import hopsworks
 
-STATION_ID = 98230
-PARAM = 1
-URL = f"https://opendata-download.smhi.se/stream?type=metobs&parameterIds={PARAM}&stationId={STATION_ID}&period=latest-months"
 
 def fetch_dataframe():
-    temp_df = pd.read_csv(URL, delimiter=';', skiprows=9)
+    subprocess.run(["./fetch"], cwd=os.path.join(os.getcwd(), "./data/")) 
 
-    temp_df = temp_df.drop(columns=["Unnamed: 4", "Tidsutsnitt:"])
-    temp_df["Date-Time"] = pd.to_datetime(temp_df["Datum"] + " " + temp_df["Tid (UTC)"])
-    temp_df = temp_df.drop(columns=["Datum", "Tid (UTC)"])
-    temp_df.sort_values("Date-Time")
-    temp_df.drop_duplicates()
+    temp_df = None
+
+    for fid in range(101):
+        try:
+            archive_df = pd.read_csv(f"./data/97100-{fid}-archive.csv", delimiter=';')
+            latest_df = pd.read_csv(f"./data/97100-{fid}-latest.csv", delimiter=';')
+        except FileNotFoundError:
+            continue
+        tmp_df = pd.concat([archive_df, latest_df])
+        print(f"=== {fid} ===")
+        print(tmp_df.info())
+        tmp_df = tmp_df.drop(columns=["Kvalitet", "Unnamed: 4", "Tidsutsnitt:"])
+        tmp_df["Date-Time"] = pd.to_datetime(tmp_df["Datum"] + " " + tmp_df["Tid (UTC)"])
+        tmp_df = tmp_df.drop(columns=["Datum", "Tid (UTC)"])
+        print(tmp_df.head())
+        if temp_df is None:
+            temp_df = tmp_df
+        else:
+            temp_df = pd.merge(temp_df, tmp_df, how="outer", on="Date-Time")
+
+    temp_df = temp_df.set_index("Date-Time")
+
+    temp_df = temp_df.sort_values("Date-Time")
+    temp_df = temp_df.drop_duplicates()
+    temp_df = temp_df.interpolate()
+    temp_df = temp_df.dropna()
+    temp_df = temp_df.reset_index()
+    temp_df["ID"] = temp_df.index
 
     new_names = {}
     for name in temp_df.columns:
-        new_names[name] = name.lower().replace('-', '_')
+        tmp_name = name.lower().replace('-', '_').replace(' ', '_')
+        tmp_name = tmp_name.encode('ascii',errors='ignore')
+        new_names[name] = tmp_name.decode()
     temp_df = temp_df.rename(columns=new_names)
+    temp_df.columns
 
     return temp_df
 
@@ -26,7 +50,7 @@ def main():
     project = hopsworks.login()
     fs = project.get_feature_store()
 
-    temp_fg = fs.get_feature_group(name="weather",version=1)
+    temp_fg = fs.get_feature_group(name="weather",version=2)
     temp_df = fetch_dataframe()
     print(temp_df.info())
     print(temp_df.head())
