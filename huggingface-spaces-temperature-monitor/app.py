f79627329f8e10d38b4a11af19fe39508babda46
Oxel40 <Oxel40@users.noreply.github.com>
2024-01-05 13:58:03 +0100

    update monitor app

--- huggingface-spaces-temperature-monitor/app.py
+++ huggingface-spaces-temperature-monitor/app.py
@@ -8,15 +8,15 @@ fs = project.get_feature_store()
 dataset_api = project.get_dataset_api()
 
 dataset_api.download("Resources/images/df_recent.png")
-dataset_api.download("Resources/images/confusion_matrix.png")
+dataset_api.download("Resources/images/recent_temp_prediction_mses.png")
 
 with gr.Blocks() as demo:    
     with gr.Row():
       with gr.Column():
-          gr.Label("Recent Prediction History")
+          gr.Label("Recent Prediction MSE History")
           input_img = gr.Image("df_recent.png", elem_id="recent-predictions")
       with gr.Column():          
-          gr.Label("Confusion Maxtrix with Historical Prediction Performance")
-          input_img = gr.Image("confusion_matrix.png", elem_id="confusion-matrix")        
+          gr.Label("Historical Prediction Performance (MSE)")
+          input_img = gr.Image("recent_temp_prediction_mses.png", elem_id="mse-graph")        
 
 demo.launch()
